3191fd05da398086c173b6a9f30a66cf4f2a1563
gauravmishraitbhu <gauravmishra.itbhu@gmail.com>
2015-10-12 17:42:03 +0530

    missed file to commit

---
+++ Restreaming/Restreaming/Config.h
@@ -0,0 +1,18 @@
+//
+//  Config.h
+//  Restreaming
+//
+//  Created by gaurav on 10/09/15.
+//  Copyright (c) 2015 gaurav. All rights reserved.
+//
+
+#ifndef Restreaming_Config_h
+#define Restreaming_Config_h
+
+const char *output_path = "/Users/matthew/VideoText/out/output.mp4";
+const char *input_path = "/Users/matthew/VideoText/out/input.mp4";
+const char *frames_path = "/Users/matthew/VideoText/phantomjs-2.0.0-macosx/examples/frames/";
+
+
+
+#endif
